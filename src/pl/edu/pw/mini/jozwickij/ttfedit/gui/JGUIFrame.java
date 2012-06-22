541011ac4631db56793a17c3cda2a0e3ba8d8294
Peter Reid <peter.d.reid@gmail.com>
2012-06-21 23:10:57 -0500

    Stopped trying to reference some Mac GUI thing

--- src/pl/edu/pw/mini/jozwickij/ttfedit/gui/JGUIFrame.java
+++ src/pl/edu/pw/mini/jozwickij/ttfedit/gui/JGUIFrame.java
@@ -5,10 +5,6 @@ import javax.swing.UIManager;
 import pl.edu.pw.mini.jozwickij.ttfedit.util.Debug;
 import pl.edu.pw.mini.jozwickij.ttfedit.util.Util;
 
-import com.jgoodies.looks.Options;
-import com.jgoodies.looks.plastic.Plastic3DLookAndFeel;
-import com.jgoodies.looks.plastic.PlasticLookAndFeel;
-
 public class JGUIFrame extends GUIFrame {
 
 	private static final long serialVersionUID = 1L;
@@ -16,13 +12,7 @@ public class JGUIFrame extends GUIFrame {
 	@Override
 	protected void initialize() {
 		try {
-			if (!Util.isMac()) {
-				PlasticLookAndFeel.set3DEnabled(true);
-			    Options.setPopupDropShadowEnabled(true);
-				Options.setUseNarrowButtons(true);
-				Options.setUseSystemFonts(true);
-				UIManager.setLookAndFeel(new Plastic3DLookAndFeel());
-			}			
+					
 		}
 		catch (Exception e) {
 			Debug.printlnErr("Error while initializing look and feel: "+e,this);
